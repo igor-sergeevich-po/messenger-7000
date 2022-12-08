5afc16482b35fa7bcf65af44d0394e3b82bf70cf
Igor_PO <89515076779@ya.ru>
2022-12-08 12:20:12 +0300

    add layout for find-user

--- src/conponents/Search.jsx
+++ src/conponents/Search.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export const Search = () => {
   return (
-    <div>Search</div>
+    <div className='search'>
+        <input type="search_text" placeholder='Find user ?'/>
+        <div className="search_user">
+            <img className='search_avatar' src="https://w7.pngwing.com/pngs/805/207/png-transparent-account-avatar-profile-user-avatars-icon.png" alt="" />
+            <span className="search_name">Lord Dev</span>
+        </div>
+        
+    </div>
   )
 }
--- src/conponents/Sidebar.jsx
+++ src/conponents/Sidebar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Navbar } from './Navbar'
+import { Search } from './Search'
 
 export const Sidebar = () => {
   return (
     <div className='sidebar'>
         <Navbar />
+        <Search />
     </div>
   )
 }
