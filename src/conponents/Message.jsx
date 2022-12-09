00f4f1f89b9485b8c3624ffd893879883b73b0f7
Igor_PO <89515076779@ya.ru>
2022-12-09 12:35:11 +0300

    change layout for messages

--- src/conponents/Message.jsx
+++ src/conponents/Message.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
+import avatar from '../assets/Avatar-PNG-Image.png';
 
 export const Message = () => {
   return (
-    <div className='mes-item'>Message 1</div>
+    <div className='message'>
+      <div className="message-info">
+        <div className="message-title">
+          <img src={avatar} />
+          <span>current time</span>
+        </div>
+        <div className="message-text">
+          <p>hi man whats'up?</p>
+        </div>
+      </div>
+    </div>
   )
 }
