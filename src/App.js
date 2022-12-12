a80d965958776a88b63e71bd1d57f858e5861441
Igor_PO <89515076779@ya.ru>
2022-12-12 10:49:31 +0300

    add protect for home-page

--- src/App.js
+++ src/App.js
@@ -4,19 +4,38 @@ import { Login } from './pages/Login';
 import { Registration } from './pages/Registration';
 
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import './style.scss'
+import { useContext } from 'react';
+import { AuthContext } from './hoc/AuthContext';
 // import './App.css';
 
+
 function App() {
+  const {currentUser} = useContext(AuthContext);
+  console.log(currentUser)
+
+  const ProtectedRoute = ({children}) => {
+    if (!currentUser) {
+      alert('для начала зарегистрируйся')
+      return <Navigate to='/messenger-7000/' />
+    } else {
+      return children
+    }
+  }
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path='messenger-7000/'>
           <Route index element={<Registration />} />
           <Route path='/messenger-7000/login' element={<Login />} />
-          <Route path='/messenger-7000/home' element={<Home />} />
+          <Route path='/messenger-7000/home' element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+            } />
         </Route>
       </Routes>
     </BrowserRouter>
